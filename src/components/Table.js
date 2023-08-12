37bbcf55eaab1e3fd356568ec497eced04238fbe
dhana0208 <techdhana33@gmail.com>
2023-08-12 15:37:25 -0500

    assigmnment 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,34 @@
+import "./styling.css";
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+        
+          <th>Name</th>
+          <th>Type of Satelite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+        
+          {
+            sat.map((st)=>{
+     
+              return <tr key={st.id}>
+                 <td>{st.name}</td>
+                 <td>{st.type}</td>
+                 <td>{st.launchDate}</td>
+                 <td>{st.operational?"Active":"In Active"}</td>
+              
+              </tr>
+            })
+
+
+          }
+         
         </tbody>
       </table>
   );
